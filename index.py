85949f22df79ca89d409d10a3b8bf6231fb7c168
SoyamDarshan <soyam.darshan@gmail.com>
2019-07-28 21:46:50 +0530

    heroku deploy 4

--- index.py
+++ index.py
@@ -38,11 +38,14 @@ class Index(object):
 if __name__ == '__main__':
     conf = {"/static/css": {"tools.staticdir.on": True,
                             "tools.staticdir.dir": os.path.abspath("./static/css/"), },
-            'host_and_port': {'server.socket_host': '0.0.0.0',
-                              'server.socket_port': 8090,
-                              'engine.autoreload.on': False,
-                              'log.access_file': './access.log',
-                              'log.error_file': './error.log'}
-            }
+            'global': {
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', 5000)),
+    },
+        '/': {
+        'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+    },
+    }
     # print(conf)
     cherrypy.quickstart(Index(), '/', config=conf)
